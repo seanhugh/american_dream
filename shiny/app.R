38dd8dc834bc7071e59648286ada1fb4e25bda8e
Sean Hughes <shughes@college.harvard.edu>
2018-12-02 01:16:01 -0500

    adding new data for inequality, added new content to shiny app

--- shiny/app.R
+++ shiny/app.R
@@ -7,9 +7,10 @@ library(shinydashboard)
 
 # Read in Data for charts from RDS files
 
-# chart 1
 chart1 <- read_rds("chart1.rds")
 chart2 <- read_rds("chart2.rds")
+chart3 <- read_rds("chart3.rds")
+chart4 <- read_rds("chart4.rds")
 
 
 
@@ -18,7 +19,8 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Home", tabName = "dashboard", icon = icon("home")),
-      menuItem("Income Bracket", tabName = "income", icon = icon("dollar"))
+      menuItem("Income Bracket", tabName = "income", icon = icon("dollar")),
+      menuItem("Time Period", tabName = "time", icon = icon("calendar"))
     )
   ),
   dashboardBody(
@@ -47,7 +49,12 @@ ui <- dashboardPage(
                 their parents by income bracket. It is clear that higher children from families with higher income brackets 
                 almost always have a higher percentage chance of earning more than their parents than those
                 from low income brackets. We can plot a few key comparisons below to further demonstrate these
-                changes over time.")
+                changes over time."),
+              plotOutput("plot3"),
+              p("It is interesting to see the sharp increase in income disparities during the 1940 - 1954 
+                time period, compared with the gradual slope from 1954 onwards (disparity decreases between the
+                top percentile and the bottom percentile during this period)."),
+              plotOutput("plot4")
       )
     )
   )
@@ -57,7 +64,7 @@ server <- function(input, output) {
   
   # Chart 1 Plot
   output$plot1 <- renderPlot({
-    ggplot(simple_plot, aes(x = cohort, y = cohort_mean)) + 
+    ggplot(chart1, aes(x = cohort, y = cohort_mean)) + 
       geom_line(size = .75) + geom_point(size = 3) + 
       coord_cartesian(ylim = c(.48, 1.00)) +
       scale_y_continuous(labels = scales::percent) +
@@ -69,7 +76,7 @@ server <- function(input, output) {
             axis.title.y = element_text(colour = "#9d9eac"))
   })
   
-  # Chart 1 Plot
+  # Chart 2 Plot
   output$plot2 <- renderPlot({
     ggplot(chart2, aes(x = cohort, 
                                   y = probability_exceed, 
@@ -86,6 +93,27 @@ server <- function(input, output) {
       theme_bw() +
       theme(axis.title.x = element_text(colour = "#9d9eac"),
             axis.title.y = element_text(colour = "#9d9eac"))
+  })
+  
+  # Chart 3 Plot
+  output$plot3 <- renderPlot({
+    ggplot(chart3, aes(x = cohort, y = data, color = Percentile)) + 
+      geom_line(size = 1) +
+      labs(title = "Disparities Between Percentage Chances of Earning More than Parents",
+           y = "Dif. between top x percentile and bottom x percentile",
+           x = "Year")
+  })
+  
+  # Chart 4 Plot
+  output$plot4 <- renderPlot({
+    chart4 %>% 
+      ggplot(aes(x = year)) + 
+      geom_line(aes(y = percentage.dif, colour = "Top vs. Bottom 10% Disparity")) +
+      geom_line(aes(y = percentage.inequality * 2.5, colour = "Wealth Held by Top 1%")) +
+      scale_y_continuous(sec.axis = sec_axis(~./2.5, name = "% of Wealth Heald by top 1%")) +
+      labs(title = "Disparities Between Top and Bottom 10th Percentile vs. Income Inequality",
+           y = "Dif. between top 10% percentile and bottom 10% percentile",
+           x = "Year")
   })
   
 }
